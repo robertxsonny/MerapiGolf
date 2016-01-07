ddddfdbe01caeb53ecdead1d8b1c22d336a421c8
theodorusyoga <theodorusyoga_ti09@mail.ugm.ac.id>
2016-01-07 13:00:37 +0700

    udah bisa pilih kategori + shortcut2 di dalam aplikasi

---
+++ MerapiGolfLogistik/MerapiGolfLogistik/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MerapiGolfLogistik.Models
+{
+    public class CategoryView : MerapiGolfLogistik.Category
+    {
+       public int stock { get; set; }
+    }
+}
