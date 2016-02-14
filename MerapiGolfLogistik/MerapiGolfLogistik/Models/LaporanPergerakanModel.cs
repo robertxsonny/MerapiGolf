4fb0eccc6ab9c1280eaea203b5f12a2bd02b83b0
theodorusyoga <theodorusyoga_ti09@mail.ugm.ac.id>
2016-02-15 00:54:41 +0700

    baru buat class pergerakan model

---
+++ MerapiGolfLogistik/MerapiGolfLogistik/Models/LaporanPergerakanModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MerapiGolfLogistik.Models
+{
+    public enum TipePergerakan
+    {
+        Pembelian,
+        Pengambilan
+    }
+
+    public class LaporanPergerakanModel
+    {
+        public Guid id_kategori { get; set; }
+        public string subsi_kategori { get; set; }
+        public string nama_kategori { get; set; }
+        public Guid id_barang { get; set; }
+        public string subsi_barang { get; set; }
+        public string nama_barang { get; set; }
+    }
+}
